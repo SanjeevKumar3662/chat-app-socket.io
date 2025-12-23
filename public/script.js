ef0c1e951e79c287aa798a7c75aafa1c3ff1c30b
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-12-23 13:05:46 +0530

    Refactor HTML form structure and enhance message handling in JavaScript

--- public/script.js
+++ public/script.js
@@ -1,2 +1,25 @@
 // console.log(io);
+console.log("running ?");
+
+//io() connectes to the socket.io server at the url
 const socket = io("http://localhost:3000");
+
+//out socket has an on method
+// and an emit method
+
+socket.on("welcome", (data) => {
+  console.log("data", data);
+  // once welcom is emitted from the server, we run this callback
+  // socket.emit("thankYou", [4, 5, 6]);
+});
+
+const inputBtn = document.querySelector(".btn-primary");
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  const input = document.querySelector("#user-message");
+  console.log(input.value);
+  socket.emit("thankYou", input.value);
+};
+
+inputBtn.addEventListener("click", sendMessage);
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import express from "express";
+import { createServer } from "http";
+
 import { Server } from "socket.io";
 
 const app = express();
-const PORT = 3000;
-
 app.use(express.static("public"));
-const expressServer = app.listen(PORT);
 
-// console.log(expressServer);
+const server = createServer(app);
+const io = new Server(server);
 
-const io = new Server(expressServer, {});
-// console.log(io);
+app.get("/", (_, res) => {
+  res.sendFile("/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id, "has joined us");
+  // 1st arg of emit is the event name, 2nd is the data we want to provide
+  socket.emit("welcome", [1, 2, 3]);
+
+  socket.on("thankYou", (data) => {
+    console.log("other data", data);
+  });
+});
+server.listen(3000, () => {
+  console.log("server running");
+});
