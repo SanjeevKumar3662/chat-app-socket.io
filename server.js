7370750ca6a79d8e09565f090041d49324faeac4
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-12-24 15:40:50 +0530

    Add chat functionality with user management and message handling

--- server.js
+++ server.js
@@ -1,28 +1,13 @@
 import express from "express";
-import { createServer } from "http";
-
-import { Server } from "socket.io";
 
 const app = express();
-app.use(express.static("public"));
 
-const server = createServer(app);
-const io = new Server(server);
+app.use(express.static("public"));
 
 app.get("/", (_, res) => {
   res.sendFile("/index.html");
 });
 
-io.on("connection", (socket) => {
-  // 1st arg of emit is the event name, 2nd is the data we want to provide
-  console.log(socket.id, "has joined us");
-  socket.emit("welcome", "Welcome to the chat!");
-
-  socket.on("toServer", (data) => {
-    console.log("toServer", data);
-    io.emit("welcome", data);
-  });
-});
-server.listen(3000, () => {
-  console.log("server running");
+app.listen(3000, () => {
+  console.log("Server is listening at PORT:3000");
 });
