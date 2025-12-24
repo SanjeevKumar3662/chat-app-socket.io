3e4022a9a0599a6512f9d06c05489ba3943deae5
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-12-24 17:19:07 +0530

    Implement chat message handling and format message utility

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 import express from "express";
+import { Server } from "socket.io"; //
+import { createServer } from "http";
+import formatMessage from "./utils/messages.js";
+
+const PORT = 3000;
+const botName = "Our Bot";
 
 const app = express();
 
 app.use(express.static("public"));
 
+const server = createServer(app);
+const io = new Server(server); //
+
+io.on("connection", (socket) => {
+  console.log(`New socket connected : ${socket.id}`);
+
+  socket.emit("message", formatMessage(botName, "Did you get my message"));
+
+  //Brodcast when a user connects
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
+
+  //Brodcast when a user disconects
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
+  });
+
+  socket.on("chatMessage", (data) => {
+    console.log(data);
+    io.emit("message", formatMessage("user", data));
+  });
+});
+
 app.get("/", (_, res) => {
   res.sendFile("/index.html");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening at PORT:3000");
+server.listen(PORT, () => {
+  //
+  console.log("Server is listening at PORT : ", PORT);
 });
