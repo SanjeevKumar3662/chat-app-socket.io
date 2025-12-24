f52261e288d9a5d31f3beb5edca442c187660f02
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-12-24 18:58:34 +0530

    Add user management functions for chat application

---
+++ utils/users.js
@@ -0,0 +1,28 @@
+const users = [];
+
+// Join user to chat
+
+export const userJoin = (id, username, room) => {
+  const user = { id, username, room };
+  users.push(user);
+
+  return user;
+};
+
+export const userLeave = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+export const getRoomUsers = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+//Get current user
+
+export const getCurrentUser = (id) => {
+  return users.find((user) => user.id === id);
+};
